71d57a61082d73b74b4b64fad3b3715bfab9f68c
ununique3 <j.horst77@gmail.com>
2022-09-11 10:46:10 -0600

    issues

--- sequencer_api/models.py
+++ sequencer_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-#when updating model, make sure to update serializers.py and views.py
+#when updating model, make sure to update serializers.py
 class Sequence(models.Model):
     name = models.CharField(max_length=32, null=True)
     steps = models.IntegerField(null=True)
